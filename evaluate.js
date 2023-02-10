2eebb7fdeb7d6d5cce2a9208aa5f0e0f85100ef5
128bit <128bit@eclipso.eu>
2023-02-10 15:17:47 +0300

    Add division/multiplication handling

--- evaluate.js
+++ evaluate.js
@@ -7,6 +7,22 @@ function evaluate(expr) {
     if (expr.match(/-/))
         return expr.split("-").map(el => evaluate(el)).reduce((total, num) => total - num);
 
+    const operators = expr.split(/[*/]/);
+    const operands = expr.match(/[/*]/g);
+    let total = operators.shift();
+
+    for (let i of operators) {
+        switch (operands.shift()) {
+            case "*":
+                total *= i;
+                break;
+            case "/":
+                total /= i;
+                break;
+        }
+    }
+
+    return total;
 }
 
-// console.log(evaluate("2+2-6"));
+console.log(evaluate("2+2*2"));
