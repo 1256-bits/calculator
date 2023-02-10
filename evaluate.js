7943cdbf41494a5264719fbdd56fa6bde2e044ea
128bit <128bit@eclipso.eu>
2023-02-10 14:52:35 +0300

    add addition/substitution handling

--- evaluate.js
+++ evaluate.js
@@ -1,6 +1,12 @@
 function evaluate(expr) {
     if (!isNaN(+expr))
-        return expr;
+        return +expr;
 
-    
+    if (expr.match(/\+/))
+        return expr.split("+").map(el => evaluate(el)).reduce((total, num) => total + num);
+    if (expr.match(/-/))
+        return expr.split("-").map(el => evaluate(el)).reduce((total, num) => total - num);
+
 }
+
+// console.log(evaluate("2+2-6"));
