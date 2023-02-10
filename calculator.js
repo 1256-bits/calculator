e49b7a51ea2ea5e0d5586a2d9af45314bdb9bbf1
128bit <128bit@eclipso.eu>
2023-02-10 18:01:20 +0300

    Implement clear, backspace, evaluate buttons.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,35 @@
 const screen = document.querySelector(".screen");
 const numKeys = document.querySelectorAll(".numpad input");
-const operators = document.querySelectorAll("")
+// const operators = document.querySelectorAll("")
+const bspaceKey = document.querySelector("#backspace");
+const clearKey = document.querySelector("#clear");
+const evalKey = document.querySelector("#eval");
+
+/*
+    TODO:
+    1) Working operands
+    2) Can't add more than one "." per number
+    3) "." adds 0 if used in the beginning
+    4) "=" calls evaluate
+*/
+
+numKeys.forEach(key => key.addEventListener("click", fillScreen));
+bspaceKey.addEventListener("click", backspace);
+clearKey.addEventListener("click", clearScreen);
+evalKey.addEventListener("click", () => {
+    screen.innerText = evaluate(screen.innerText);
+});
+
+function fillScreen(e) {
+    screen.innerText += e.target.value;
+}
+
+function backspace() {
+    screen.innerText = screen.innerText.slice(0, -1);
+}
+
+function clearScreen() {
+    screen.innerText = "";
+}
+
+
